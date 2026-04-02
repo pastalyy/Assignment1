ea631955b932f3fafc24fcaab77809124ae44214
pastalyy <nicholasbrown0608@gmail.com>
2026-04-03 02:32:43 +1030

    Creating sum of numbers
    Numbers are summed and put into a0

---
+++ calc.s
@@ -0,0 +1,48 @@
+.data
+
+element1: .word 1
+
+element2: .word 2
+
+element3: .word 3
+
+element4: .word 4
+
+element5: .word 5
+
+num_elements: .word 5
+
+#...to here---
+
+.text
+
+main:
+
+#load memory addresses into temporary registers
+
+la t0, element1
+la t1, element2
+la t2, element3
+la t3, element4
+la t4, element5
+
+#loads value from address
+
+lw t0, 0(t0)
+lw t1, 0(t1)
+lw t2, 0(t2)
+lw t3, 0(t3)
+lw t4, 0(t4)
+
+#sums numbers into s0
+
+add s0, t0, t1
+add s0, s0, t2
+add s0, s0, t3
+add s0, s0, t4
+
+done:
+
+ li a7, 10
+
+ ecall
